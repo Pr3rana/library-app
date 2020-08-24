3ae472237978a8b375226698db4f808d85b4a1b3
Prerna Singh <prernasingh216@gmail.com>
2020-08-24 23:58:17 +0530

    New: Added autosuggest panel

---
+++ src/autosuggestPanel/Autosuggest.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Autosuggest.css'
+
+/*template for row in autosuggest panel*/
+function SuggestionRow({value, click}){
+  return(
+    <div className="suggestionRow" onClick={click}>
+      {value}
+    </div>
+  )
+}
+// list: [{label, id}]
+function AutoSuggestPanel ({list = [], onClick}) {
+  return(
+    <div id="autosuggestPanel">
+      {
+        /*Mapping the relevancy search result to  autosuggest*/
+      list.map((el, index) => {
+        return (<SuggestionRow 
+          key = {`${el}${index}`}
+          click={() => onClick(el)} 
+          value = {el.label} 
+        />)
+      })
+      }
+    </div>
+  )
+}
+
+export default AutoSuggestPanel
